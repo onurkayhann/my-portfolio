5a842cb2b4c65c9830d34495408370935a0a2712
Onur Kayhan <onur.kayhan@chasacademy.se>
2022-06-21 00:49:14 +0200

    added my technologies

---
+++ src/helpers/techs.js
@@ -0,0 +1,38 @@
+export const techs = [
+  {
+    name: 'React',
+    iconName: 'react'
+  },
+  {
+    name: 'JavaScript',
+    iconName: 'javascript'
+  },
+  {
+    name: 'HTML 5',
+    iconName: 'html5'
+  },
+  {
+    name: 'CSS 3',
+    iconName: 'css3'
+  },
+  {
+    name: 'Mongo DB',
+    iconName: 'mongodb'
+  },
+  {
+    name: 'Bootstrap',
+    iconName: 'bootstrap'
+  },
+  {
+    name: 'Node JS',
+    iconName: 'nodejs'
+  },
+  {
+    name: 'Docker',
+    iconName: 'docker'
+  },
+  {
+    name: 'Github',
+    iconName: 'github'
+  },
+]
