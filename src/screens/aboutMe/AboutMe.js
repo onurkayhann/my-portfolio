59c6d3bf4145beb7f4f4a09224c1aec41a0b97c7
Onur Kayhan <onur.kayhan@chasacademy.se>
2022-06-21 19:54:47 +0200

    final add, update aboutMe

--- src/screens/aboutMe/AboutMe.js
+++ src/screens/aboutMe/AboutMe.js
@@ -1,21 +1,29 @@
-import React from 'react';
-import './aboutMe.css';
-import me from '../../images/me.png';
+import React from "react";
+import "./aboutMe.css";
+import me from "../../images/me.png";
 
 function AboutMe() {
   return (
     <div className="about__container" id="about-me">
       <div>
-      <img src={me} alt="me onur icon" />
+        <img src={me} alt="me onur icon" />
       </div>
-        <div className="about__text">
-          <h1>ABOUT ME</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        
-
+      <div className="about__text">
+        <h1>ABOUT ME</h1>
+        <p>
+          A Fullstack Developer student based in Stockholm, Sweden. I have a
+          huge passion for web development and coding, I love new challenges,
+          and I always strive to improve myself in other areas as well. I am in
+          general a very curious guy that wants to experience and learn new
+          things. My other passions include: Learning new languages, travels &
+          explore new cultures, reading books & learning. I am an active guy
+          that workouts regularly, and as well learning the Brazilian martial
+          art called Brazilian Jiu-Jiutsu, and of course try new dishes from all
+          around the world.
+        </p>
       </div>
     </div>
-  )
+  );
 }
 
 export default AboutMe;
