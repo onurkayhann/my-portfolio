804483cbcf4cc74ea78263c73b6a4d89f1049b20
Onur Kayhan <onur.kayhan@chasacademy.se>
2022-06-21 02:00:02 +0200

    add portoflio data, portfolio, and project icons

---
+++ src/helpers/portfolioData.js
@@ -0,0 +1,29 @@
+import hangmanGame from '../images/projectsicon/hangmanGame.png';
+import weatherApp from '../images/projectsicon/weatherApp.png';
+import planetTrip from '../images/projectsicon/planetTrip.png';
+
+const portfolioData = [
+  {
+    image: planetTrip,
+    link: 'https://helloworldstraveling.netlify.app/',
+    desc: 'Planet trip app',
+    summary: 'An app that allows you to book trips to different planets',
+    tech: 'React | Typescript | Bootstrap | Node | Express | Mongo DB'
+  },
+  {
+    image: weatherApp,
+    link: 'https://weather-one.netlify.app/',
+    desc: 'Weather app',
+    summary: 'An app that locates your actual location to give you some weather data',
+    tech: 'React'
+  },
+  {
+    image: hangmanGame,
+    link: 'https://github.com/onurkayhann/Hangman-Game',
+    desc: 'Hangman game',
+    summary: 'An app that is build with vanilla JS to play the traditional hangman game',
+    tech: 'HTML5 | CSS3 | JavaScript'
+  }
+]
+
+export default portfolioData;
---
+++ src/screens/portfolio/Portfolio.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './portfolio.css';
+import portfolioData from '../../helpers/portfolioData';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+
+function Portfolio() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Portfolio;
